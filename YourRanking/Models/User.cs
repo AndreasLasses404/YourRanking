cf20a76ca772a06a25972736b90e02d447dc9413
AndreasLasses404 <56150531+AndreasLasses404@users.noreply.github.com>
2021-04-05 11:01:46 +0200

    first commit

---
+++ YourRanking/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YourRanking.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public int UserName { get; set; }
+
+    }
+}
